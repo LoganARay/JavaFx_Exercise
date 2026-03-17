a8e2a1feb5edcd4497a721b5e262601070096917
Logan <mewtoo857@gmail.com>
2026-03-17 11:37:35 -0400

    This is the final one NO IDEA WHAT HAPPENED AGAIN so sorry

--- src/main/java/edu/farmingdale/javafx_exercise/HelloApplication.java
+++ src/main/java/edu/farmingdale/javafx_exercise/HelloApplication.java
@@ -23,3 +23,23 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+//.column-header {
+//    -fx-background-color: #A3A3A3;
+//}
+//
+//.column-header .label {
+//    -fx-text-fill: white;
+//    -fx-font-weight: bold;
+//}
+//.form-input {
+//    -fx-background-color: white;
+//    -fx-text-fill: black;
+//    -fx-prompt-text-fill: #888888;
+//    -fx-border-color: transparent;
+//    -fx-background-radius: 4;
+//    -fx-border-radius: 4;
+//}
+//.form-input .content {
+//    -fx-background-color: blue;
+//    -fx-background-radius: 4;
+//}
